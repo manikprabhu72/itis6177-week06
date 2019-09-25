5eca46c42cbcdfafb3eb9390d9d7d7d5d485f9d5
System Administrator <root@Apples-MacBook-Pro.local>
2019-09-24 21:27:18 -0400

    Added Changes to list component

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  public employeeList= [];
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
+    this.http.get('http://68.183.144.13:3000/employees').subscribe((resp) => {
+      this.employeeList = resp.response; 
+      console.log(resp);
+    })
   }
 
 }
